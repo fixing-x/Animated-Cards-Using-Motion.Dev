0e3d935da76a8ce4e23a42f6b19dea2b902b214c
Ajay Sharma <traversities@gmail.com>
2025-10-01 12:56:57 +0530

    Added hover effect and timing variable for animation duration

--- script.js
+++ script.js
@@ -1,39 +1,78 @@
-import { animate, press,hover, stagger } from "https://cdn.jsdelivr.net/npm/motion@12.23.12/+esm";
+import { animate, press, hover, stagger } from "https://cdn.jsdelivr.net/npm/motion@12.23.12/+esm";
 
 // Reusable press animation
 function addPressAnimation(element) {
     const card = element.querySelector(".card");
     const shadow = element.querySelector(".shadow");
+    var distance = 5;
+var timing = 0.1;
 
     const Pressed = [
-        [card,{x: 5, y: 5},{duration: 0.1}], [shadow,{x: -5, y: -5}, {duration: 0.1, start: 0, at: 0}]
+        [
+            card, 
+            { 
+                x: distance, 
+                y: distance 
+            }, { 
+                duration: timing 
+            }
+        ], [
+            shadow, 
+            { 
+                x: -distance, 
+                y: -distance 
+            }, { 
+                duration: timing,
+                start: 0, 
+                at: 0 
+            }
+        ]
     ]
     const Released = [
-        [card,{x: 0, y: 0},{duration: 0.1}], [shadow,{x: 0, y: 0},{duration: 0.1, start: 0, at: 0}]
+        [
+            card, 
+            { 
+                x: 0, 
+                y: 0 
+            }, { 
+                duration: timing 
+            }
+        ], [
+            shadow, 
+            { 
+                x: 0,
+                y: 0 
+            }, 
+            { 
+                duration: timing, 
+                start: 0, 
+                at: 0 
+            }
+        ]
     ]
     if (!card) return;
-    // hover(card, () => {
-    //     animate(card, { backgroundImage: "url('bg.mp4')" }, { duration: 0.2 });
-    //     return () => animate(card, { backgroundImage: "linear-gradient(to bottom, #000, #fff)" }, { duration: 0.1 });
-    // });
-
     hover(card, () => {
+        animate([[card, { x: -2, y: -2, borderWidth: '2px' }, { duration: 0.05 }], [shadow, { x: 2, y: 2 }, { duration: 0.05, start: 0, at: 0 }]]);
+        return () => animate([[card, { x: 0, y: 0, borderWidth: '1px' }, { duration: 0.05 }], [shadow, { x: 0, y: 0 }, { duration: 0.05 }]]);
+    });
+
+    press(element, () => {
         animate(Pressed);
-    
+
         return () => animate(Released);
     });
 }
 document.querySelectorAll('.container').forEach(addPressAnimation);
 
 // Entrance animation for cards
-animate(".cards .container", 
-    { opacity: [0, 1], y: [50, 0] }, 
-    { 
-         ease: "easeOut", 
+animate(".cards .container",
+    { opacity: [0, 1], y: [50, 0] },
+    {
+        ease: "easeOut",
         // stiffness: 80,
         // damping: 10,
         // mass: 1,
-         duration: 0.5, 
-        delay: stagger(0.1) 
+        duration: 0.5,
+        delay: stagger(0.1)
     });
 // Apply to all cards
